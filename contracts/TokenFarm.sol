c34821f6f1b6d42933c9bc7ac2a3b86a30f661d4
Edward Casanova <cb.inv7613@gmail.com>
2021-07-06 10:17:06 -0500

    Added basic data structures for stakers

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -9,8 +9,28 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    //1. Stakes Tokens (Deposit)
+    function stakeTokens(uint256 _amount) public {
+        // Transfer Mock Dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        // Update staking balance
+        stakingBalance[msg.sender] += _amount;
+
+        // Add user to stakers array *only* if they haven't staked already
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+    }
+    //2. Unstaking Tokens (Withdraw)
+    //3. Issuing tokens
 }
